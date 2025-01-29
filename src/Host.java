aced08b036d6d4786b5f7771951cab7158b118df
Dustin Muse <Dustin.muse12@gmail.com>
2025-01-29 12:30:49 -0500

    added error checking and multiple messages to be sent

--- src/Host.java
+++ src/Host.java
@@ -7,30 +7,47 @@ public class Host {
     public static void main(String[] args) throws Exception{
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Enter the source MAC: ");
-        String sourceMAC = keyboard.nextLine();
+        while(true) {
+            System.out.println("Enter the source MAC: ");
+            String sourceMAC = keyboard.nextLine();
 
-        System.out.println("Enter the destination MAC: ");
-        String destinationMAC = keyboard.nextLine();
+            while(sourceMAC.contains(",")){
+                System.out.println("You can't have a ',' in your source MAC\nEnter the source MAC: ");
+                sourceMAC = keyboard.nextLine();
+            }
 
-        System.out.println("Enter the text to send: ");
-        String text = keyboard.nextLine();
+            System.out.println("Enter the text to send: ");
+            String text = keyboard.nextLine();
 
-        //use method from parser
-        InetAddress serverIP;
-        int serverPort;
+            while(text.contains(",")){
+                System.out.println("You can't have a ',' in your text\nEnter the text to send: ");
+                text = keyboard.nextLine();
+            }
 
-        //creates full virtual packet w/ comma as a split
-        String message = sourceMAC + "," + destinationMAC + "," + text;
+            System.out.println("Enter the destination MAC: ");
+            String destinationMAC = keyboard.nextLine();
 
-        DatagramSocket socket = new DatagramSocket();
-        DatagramPacket request = new DatagramPacket(
-                message.getBytes(),
-                message.getBytes().length,
-                serverIP,
-                serverPort
-        );
-        socket.send(request);
-        System.out.println("Message sent successfully!");
+            while(destinationMAC.contains(",")){
+                System.out.println("You can't have a ',' in your destination MAC\nEnter the destination MAC: ");
+                destinationMAC = keyboard.nextLine();
+            }
+
+            //use method from parser to get IP and serverPort
+            InetAddress serverIP = null;
+            int serverPort = 3000;
+
+            //creates full virtual packet w/ comma as a split
+            String message = sourceMAC + "," + destinationMAC + "," + text;
+
+            DatagramSocket socket = new DatagramSocket();
+            DatagramPacket request = new DatagramPacket(
+                    message.getBytes(),
+                    message.getBytes().length,
+                    serverIP,
+                    serverPort
+            );
+            socket.send(request);
+            System.out.println("Message sent successfully!");
+        }
     }
 }
