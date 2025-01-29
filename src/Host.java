e591646c0624cb9db4aa7da3a6421943f8d2125c
Bagel82 <122378674+Bagel82@users.noreply.github.com>
2025-01-29 12:13:12 -0500

    Host now checks if it is the destination before printing acknowledgement.

--- src/Host.java
+++ src/Host.java
@@ -3,19 +3,22 @@ import java.net.DatagramSocket;
 import java.util.Arrays;
 public class Host {
     public static void main(String[] args) throws Exception{
+        String ID = args[0];
         int port = 3000;
         DatagramSocket receivingSocket = new DatagramSocket(port);
         DatagramPacket receivedFrame = new DatagramPacket(
                 new byte[1024], 1024);
         while(true){
             receivingSocket.receive(receivedFrame);
-            byte[] Message = Arrays.copyOf(
+            byte[] message = Arrays.copyOf(
                     receivedFrame.getData(),
                     receivedFrame.getLength()
             );
-            String[] messageData = new String(Message).split(",");
-            String acknowledgment = "Message received.\nMAC Address of sender: " + messageData[0] + ".\n Message Content: " + messageData[2];
-            System.out.println(acknowledgment);
+            String[] messageData = new String(message).split(",");
+            if(ID.equals(messageData[1])){
+                String acknowledgment = "Message received.\nMAC Address of sender: " + messageData[0] + ".\n Message Content: " + messageData[2];
+                System.out.println(acknowledgment);
+            }
         }
     }
 }
