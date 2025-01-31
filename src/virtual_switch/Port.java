e5c637d61f854a0dd7a04d0da5d029de218126b0
Max <maxwelltharp7@gmail.com>
2025-01-31 11:14:29 -0500

    finish forwardPacket function

--- src/virtual_switch/Port.java
+++ src/virtual_switch/Port.java
@@ -1,19 +1,30 @@
 package virtual_switch;
-
-import virtual_switch.Packet;
-
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Port {
     private int portID;
+    private String portName;
 
-    public Port(int portId) {
+    public Port(int portId, String portName) {
         this.portID = portId;
+        this.portName = portName;
     }
 
     // Send the packet from this port
-    public void sendPacket(Packet packet) throws IOException {
-        System.out.println("Sending packet to port " + portID + " with destination MAC " + packet.getDestMac());
+    public void forwardPacket(Packet packet) throws IOException {
+        System.out.println("Sending packet to port " + portName + " with destination MAC " + packet.getDestMac());
 
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket request = new DatagramPacket(
+                packet.getMessage().getBytes(),
+                packet.getMessage().getBytes().length,
+                InetAddress.getByName(packet.getDestMac()),
+                portID
+        );
+        socket.send(request);
+        socket.close();
     }
 }
