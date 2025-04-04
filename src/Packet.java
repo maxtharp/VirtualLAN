df10e35ef313ea9606688a9508a63ef790577611
Max <maxwelltharp7@gmail.com>
2025-04-04 11:56:03 -0400

    convert Packet to record

--- src/Packet.java
+++ src/Packet.java
@@ -1,30 +1,4 @@
-public class Packet {
-    private final String srcMac;
-    private final String destMac;
-    private final String message;
-    private final String srcIP;
-    private final String destIP;
 
-    public Packet(String srcMac, String destMac, String message, String srcIP, String destIP) {
-        this.srcMac = srcMac;
-        this.destMac = destMac;
-        this.message = message;
-        this.srcIP = srcIP;
-        this.destIP = destIP;
-    }
-    public String getSrcMac() {
-        return srcMac;
-    }
-    public String getDestMac() {
-        return destMac;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public String getSrcIP() {
-        return srcIP;
-    }
-    public String getDestIP() {
-        return destIP;
-    }
+public record Packet (String srcMac, String destMac, String message, String srcIP, String destIP, String deviceType) {
+
 }
