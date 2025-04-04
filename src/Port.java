33136ac4679cccc9f2e5986c08bf5c2a6940c150
Max <maxwelltharp7@gmail.com>
2025-04-04 11:54:46 -0400

    add method to send dv table in Port

--- src/Port.java
+++ src/Port.java
@@ -1,7 +1,11 @@
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Port {
     private final int portID;
@@ -14,10 +18,31 @@ public class Port {
 
     // Send the packet from this port
     public void forwardPacket(Packet packet, DatagramSocket receivingSocket) throws IOException {
-        System.out.println("Sending packet to port " + portID + ":" + portName + " with destination MAC " + packet.getDestMac());
+        System.out.println("Sending packet to port " + portID + ":" + portName + " with destination MAC " + packet.destMac());
 
-        String frame = packet.getSrcMac() + "," + packet.getDestMac() + "," +
-                packet.getMessage() + "," + packet.getSrcIP() + "," + packet.getDestIP();
+        String frame = packet.srcMac() + "`" + packet.destMac() + "`" +
+                packet.message() + "`" + packet.srcIP() + "`" + packet.destIP() + packet.deviceType();
+
+        DatagramPacket request = new DatagramPacket(
+                frame.getBytes(),
+                frame.length(),
+                InetAddress.getByName(portName),
+                portID
+        );
+        receivingSocket.send(request);
+    }
+
+    public void forwardDvTable(String deviceMAC, Map<String, DistanceVector> dvTable, DatagramSocket receivingSocket, String deviceType) throws IOException {
+        List<String> keys = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+        System.out.println("Sending distance vector to port " + portID + ":" + portName);
+
+        for (Map.Entry<String, DistanceVector> entry : dvTable.entrySet()) {
+            keys.add(entry.getKey());
+            values.add(entry.getValue().nextHop() + ":" + entry.getValue().cost());
+        }
+
+        String frame = deviceMAC + "`#`" + keys + ";" + values + "`#`#`" + deviceType;
 
         DatagramPacket request = new DatagramPacket(
                 frame.getBytes(),
